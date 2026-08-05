0a56ae8ab85c270252a6c66cf30384c3fae69600
Agnaldo <agnaldo15sp@gmail.com>
2026-08-05 08:57:20 -0300

    feat: add user model and database migration

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,9 @@
 from app.models.department import Department
 from app.models.employee import Employee
+from app.models.user import User
 
 __all__ = [
     "Department",
     "Employee",
+    "User",
 ]
